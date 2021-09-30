45efff3aa6d2b358b4857117343ed1d1f5ea4b23
Admin <hari01venkat@gmail.com>
2021-09-30 23:47:48 +0530

    setup commits

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import './transaction.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART CARD'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transactions
+                  .map(
+                    (tx) => Card(
+                      child: Text(tx.title),
+                    ),
+                  )
+                  .toList(),
+            )
+          ],
+        ));
+  }
+}
